9e266c7642a52610fc80122fdc9ecae741878cfc
myles <catynaimiao@foxmail.com>
2023-02-22 21:29:06 +0800

    first commit

---
+++ bmiCalculator.ts
@@ -0,0 +1,23 @@
+const calculateBmi = (centimeter: number, kilograms: number): string => {
+  if (centimeter <= 0) {
+    throw Error("centimeter must be a positive integer");
+  }
+  if (kilograms <= 0) {
+    throw Error("kilograms must be a positive integer");
+  }
+
+  const BMI = kilograms / ((centimeter / 100) * (centimeter / 100));
+
+  if (BMI > 30.0) {
+    return "Obese";
+  }
+  if (BMI > 25.0) {
+    return "Overweight";
+  }
+  if (BMI > 18.5) {
+    return "Normal";
+  }
+  return "Underweight";
+};
+
+console.log(calculateBmi(180, 74));
---
+++ exerciseCalculator.ts
@@ -0,0 +1,49 @@
+interface exercisesResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const exerciseCalculator = (
+  dailyExercises: number[],
+  target: number
+): exercisesResult => {
+  const periodLength = dailyExercises.length;
+  const trainingDays = dailyExercises.filter((hours) => hours > 0).length;
+  const totalHours = dailyExercises.reduce((acc, hours) => acc + hours, 0);
+  const averageHours = totalHours / periodLength;
+  const success = averageHours >= target;
+
+  let rating = 1;
+  if (averageHours >= target * 0.9) {
+    rating = 3;
+  } else if (averageHours >= target * 0.75) {
+    rating = 2;
+  }
+
+  const ratingDescription = {
+    1: "Needs improvement",
+    2: "Doing good",
+    3: "Excellent",
+  }[rating];
+
+  const results = {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average: averageHours,
+  };
+
+  return results;
+};
+
+const hours = [3, 0, 2, 4.5, 0, 3, 1];
+
+console.log(exerciseCalculator(hours, 2));
